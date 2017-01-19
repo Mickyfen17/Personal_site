aa9afbfe513e57cde482d00ee849e4371df46528
Michael Fenwick <mickyfen17@aol.com>
2017-01-18 19:06:18 -0700

    Add comment for es6 arrow function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$("#scroll-btn").on("click", function(e) {
+$("#scroll-btn").on("click", function(e) { //(e) => es6 arrow function
   e.preventDefault();
   $("body").animate({
     scrollTop: $("#about").offset().top
