1c8852d70294b0be8a9dae5cd9bbf718be67cc10
Michael Fenwick <mickyfen17@aol.com>
2017-04-01 00:52:55 -0600

    alter root paths again

--- main.bundle.js
+++ main.bundle.js
@@ -35080,7 +35080,7 @@
 	      { className: 'main-header-wrapper' },
 	      _react2.default.createElement(_MainTitle2.default, null),
 	      _react2.default.createElement('img', { id: 'main-macbook-img',
-	        src: '/images/macbook_pro_2.png',
+	        src: 'images/macbook_pro_2.png',
 	        alt: 'macbook type computer with text on screen' }),
 	      _react2.default.createElement('button', {
 	        id: 'scroll-btn',
@@ -35869,22 +35869,22 @@
 	var SkillsList = [{
 	  title: 'JavaScript',
 	  skills: ['ES6,', 'React,', 'React Router,', 'Redux,', 'Node,', 'jQuery,', 'Webpack.'],
-	  imagePath: '/images/code.svg',
+	  imagePath: 'images/code.svg',
 	  imageAlt: 'web browser with code symbols'
 	}, {
 	  title: 'Testing',
 	  skills: ['Mocha,', 'Chai,', 'Selenium,', 'Enzyme,', 'Jest,', 'Sinon.'],
-	  imagePath: '/images/testing_icon.svg',
+	  imagePath: 'images/testing_icon.svg',
 	  imageAlt: 'computer monitor with tick inside screen'
 	}, {
 	  title: 'Workflow',
 	  skills: ['Git,', 'Github.'],
-	  imagePath: '/images/workflow.svg',
+	  imagePath: 'images/workflow.svg',
 	  imageAlt: 'tree graph showing workflow'
 	}, {
 	  title: 'Web Design',
 	  skills: ['HTML5,', 'CSS3,', 'SCSS,', 'Bootstrap,', 'Canvas,', 'ARIA,', 'aXe.'],
-	  imagePath: '/images/design.svg',
+	  imagePath: 'images/design.svg',
 	  imageAlt: 'painters palette'
 	}];
 
@@ -35981,68 +35981,68 @@
 	var Projects = [{
 	  title: 'Weathrly',
 	  description: 'Fully accessible Weather application built in React which utilizes weatherUndergrounds API. Application was tested using Mocha, Chai, Enzyme and Sinon.',
-	  image: '/images/weathrly.png',
+	  image: 'images/weathrly.png',
 	  gitHubRepo: 'https://github.com/Mickyfen17/weathrly',
 	  siteURL: 'https://mickyfen17.github.io/weathrly/',
 	  techUsed: ['Javascript ', 'React ', 'Mocha ', 'Chai ', 'Enzyme ', 'Sinon ', 'HTML5 ', 'CSS3 ', 'ARIA'],
 	  icons: {
-	    git: '/images/github.svg',
-	    site: '/images/browser.svg'
+	    git: 'images/github.svg',
+	    site: 'images/browser.svg'
 	  }
 	}, {
 	  title: 'GameTime',
 	  description: 'Re-make of the classic Breakout arcade game using Javascript and HTML Canvas. Application was tested during build with Mocha & Chai.',
-	  image: '/images/game_time.png',
+	  image: 'images/game_time.png',
 	  gitHubRepo: 'https://github.com/Mickyfen17/game-time',
 	  siteURL: 'https://mickyfen17.github.io/game-time/',
 	  techUsed: ['Javascript ', 'HTML Canvas ', 'CSS3 ', 'Mocha ', 'Chai'],
 	  icons: {
-	    git: '/images/github.svg',
-	    site: '/images/browser.svg'
+	    git: 'images/github.svg',
+	    site: 'images/browser.svg'
 	  }
 	}, {
 	  title: 'SwapiBox',
 	  description: 'Starwars card application where we displayed various data using async promise calls and rendered the data using React',
-	  image: '/images/swapi_box.png',
+	  image: 'images/swapi_box.png',
 	  gitHubRepo: 'https://github.com/Mickyfen17/SWAPIbox',
 	  siteURL: 'https://mickyfen17.github.io/SWAPIbox/',
 	  techUsed: ['Javascript ', 'React ', 'HTML5 ', 'SCSS ', 'Jest ', 'Enzyme'],
 	  icons: {
-	    git: '/images/github.svg',
-	    site: '/images/browser.svg'
+	    git: 'images/github.svg',
+	    site: 'images/browser.svg'
 	  }
 	}, {
 	  title: '2DoBox',
 	  description: 'Fully accessible single page application using Local Storage to keep track of To-Do\'s. Similar aplication to ideaBox but refactored using ES6. Application tested using Mocha, Chai and Selenium.',
-	  image: '/images/2_do_box.png',
+	  image: 'images/2_do_box.png',
 	  gitHubRepo: 'https://github.com/Mickyfen17/2DoBox-Pivot',
 	  siteURL: 'https://mickyfen17.github.io/2DoBox-Pivot/',
 	  techUsed: ['Javascript ', 'Mocha ', 'Chai ', 'Selenium ', 'HTML5 ', 'SCSS ', 'ARIA'],
 	  icons: {
-	    git: '/images/github.svg',
-	    site: '/images/browser.svg'
+	    git: 'images/github.svg',
+	    site: 'images/browser.svg'
 	  }
 	}, {
 	  title: 'Linked List',
 	  description: 'Application written in Vanilla JS which is designed to bookmark favorite websites. Application utilizes localStorage so that the bookmark persist after page refresh.',
-	  image: '/images/linked_list.png',
+	  image: 'images/linked_list.png',
 	  gitHubRepo: 'https://github.com/Mickyfen17/Linked_List',
 	  siteURL: 'https://mickyfen17.github.io/Linked_List/',
 	  techUsed: ['Javascript ', 'HTML5 ', 'CSS'],
 	  icons: {
-	    git: '/images/github.svg',
-	    site: '/images/browser.svg'
+	    git: 'images/github.svg',
+	    site: 'images/browser.svg'
 	  }
 	}, {
 	  title: 'IdeaBox',
 	  description: 'Single page application wittren in jQuery while using Local Storage to keep track of ideas.',
-	  image: '/images/ideaBox.png',
+	  image: 'images/ideaBox.png',
 	  gitHubRepo: 'https://github.com/Mickyfen17/ideaBox',
 	  siteURL: 'https://mickyfen17.github.io/ideaBox/',
 	  techUsed: ['Javascript ', 'jQuery ', 'HTML5 ', 'CSS'],
 	  icons: {
-	    git: '/images/github.svg',
-	    site: '/images/browser.svg'
+	    git: 'images/github.svg',
+	    site: 'images/browser.svg'
 	  }
 	}];
 
