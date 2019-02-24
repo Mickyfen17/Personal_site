623357d858eb9da68234d1a44ed293e9d02d8df5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-24 07:14:52 +0000

    Deploy Mickyfen17/Personal_site to github.com/Mickyfen17/Personal_site.git:gh-pages

--- README.md
+++
@@ -1,5 +0,0 @@
-# Person Website
-
-Personal website built using React js and SCSS.
-
-View it [here](https://mickyfen17.github.io/Personal_site/)
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const commonConfig = require('./webpackUtils/webpack.common');
-const webpackMerge = require('webpack-merge');
-
-module.exports = env => {
-  const envConfig = require(`./webpackUtils/webpack.${env.env}.js`);
-
-  return webpackMerge(commonConfig, envConfig);
-};
