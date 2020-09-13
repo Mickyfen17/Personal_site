b54cd9a58d0d856a97cd27ff936aff8f95b61025
Michael Fenwick <mickyfen17@aol.com>
2020-09-13 14:22:35 -0700

    add Neon title for main header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import MainTitle from '../MainTitle';
+import NeonTitle from '../NeonTitle';
 import 'styles/header.scss';
 
 const Header = () => {
   return (
     <header>
-      <MainTitle />
+      <NeonTitle />
     </header>
   );
 };
--- src/components/MainTitle/MainTitle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MainTitle = () => (
-  <section id="main-title">
-    <h1>Mike Fenwick</h1>
-  </section>
-);
-
-export default MainTitle;
