27798b29eb17d1896e9b867f933fc6f209feee6d
Michael Fenwick <mickyfen17@aol.com>
2020-09-21 22:59:53 -0700

    add reducer fetch pattern

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect, useReducer } from 'react';
 import { NeonLightsContextProvider } from './context/NeonLightsContext';
 import NeonHeader from './components/NeonHeader';
 import About from './components/About';
@@ -6,30 +6,82 @@ import Projects from './components/Projects';
 import Footer from './components/Footer';
 import Toggle from './components/Toggle';
 
-const useFetch = url => {
-  const [apiData, setApiData] = useState({ skills: null, projects: null });
+const LOAD_DATA_SUCCESS = 'LOAD_DATA_SUCCESS';
+const LOAD_DATA_FAILED = 'LOAD_DATA_FAILED';
+
+const initialState = {
+  isLoading: true,
+  hasError: false,
+  projects: null,
+  skills: null
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case LOAD_DATA_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        hasError: false,
+        ...action.payload
+      };
+    case LOAD_DATA_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        hasError: true
+      };
+
+    default:
+      return state;
+  }
+};
+
+const formatProjects = projectsList => {
+  const projectMap = new Map();
+
+  projectsList.forEach(project => projectMap.set(project.title, project));
+  return projectMap;
+};
+
+const useAppReducer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const fetchData = async () => {
     try {
-      const response = await fetch(url);
-      const { projectData, skillData } = await response.json();
-      setApiData({ projects: projectData, skills: skillData });
+      const response = await fetch(
+        'https://uq9jwb3puh.execute-api.us-east-2.amazonaws.com/PROD/my-data'
+      );
+      const { projectData, skillData: skills } = await response.json();
+      const projects = formatProjects(projectData);
+
+      dispatch({
+        type: LOAD_DATA_SUCCESS,
+        payload: { projects, skills }
+      });
     } catch (err) {
-      throw new Error(err);
+      dispatch({ type: LOAD_DATA_FAILED });
     }
   };
 
   useEffect(() => {
-    fetchData(url);
+    fetchData();
   }, []);
-  return apiData;
+
+  return state;
 };
 
 const App = () => {
   const [lightsOn, toggleLights] = useState(true);
-  const { projects, skills } = useFetch(
-    'https://uq9jwb3puh.execute-api.us-east-2.amazonaws.com/PROD/my-data'
-  );
+  const { isLoading, hasError, projects, skills } = useAppReducer();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (hasError) {
+    return <div>Error</div>;
+  }
 
   return (
     <Fragment>
@@ -38,7 +90,7 @@ const App = () => {
           <Toggle lightsOn={lightsOn} toggleLights={toggleLights} />
           <NeonHeader>Mike Fenwick</NeonHeader>
           <About skills={skills} />
-          <Projects projectsList={projects} />
+          <Projects projectsMap={projects} />
           <Footer />
         </NeonLightsContextProvider>
       )}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,26 +3,25 @@ import AnchorLink from '../AnchorLink';
 import NeonContentWrapper from '../NeonContentWrapper';
 import 'styles/projects.scss';
 
-const Projects = ({ projectsList }) => {
-  return (
-    <section id="projects">
-      <NeonContentWrapper title="Projects">
-        {projectsList.map(({ title, gitHubRepo, siteURL }) => (
-          <div className="project">
-            <h3>{title}</h3>
-            <div>
-              <AnchorLink type="project-link" href={gitHubRepo}>
-                Code
-              </AnchorLink>
-              <AnchorLink type="project-link" href={siteURL}>
-                Live Site
-              </AnchorLink>
-            </div>
+const Projects = ({ projectsMap }) => (
+  <section id="projects">
+    <NeonContentWrapper title="Projects">
+      {Array.from(projectsMap).map(([key, { title, gitHubRepo, siteURL }]) => (
+        <div key={key} className="project">
+          <h3>{title}</h3>
+          <div>
+            <AnchorLink type="project-link" href={gitHubRepo}>
+              Code
+            </AnchorLink>
+            <AnchorLink type="project-link" href={siteURL}>
+              Live Site
+            </AnchorLink>
+            <button onClick={() => console.log(key)}>open project</button>
           </div>
-        ))}
-      </NeonContentWrapper>
-    </section>
-  );
-};
+        </div>
+      ))}
+    </NeonContentWrapper>
+  </section>
+);
 
 export default Projects;
