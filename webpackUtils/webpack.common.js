abc63a41054137944ad3fca5e000bcf09ee0fd1c
Michael Fenwick <mickyfen17@aol.com>
2021-03-13 22:37:26 -0800

    add ReactRefreshWebpackPlugin

--- webpackUtils/webpack.common.js
+++ webpackUtils/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const config = {
-  entry: ['react-hot-loader/patch', './src/'],
+  entry: './src/',
   module: {
     rules: [
       {
